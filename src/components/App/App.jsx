2f6a052670c8e83c25a6afd2b82138c5942e1e0e
nwabisadlokweni <dlonwa001@gmail.com>
2021-08-30 09:31:12 +0200

    error fixing

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,41 +6,41 @@ import { HashRouter } from "react-router-dom";
 import { Routing } from "./App.Routing";
 import { Provider as AuthProvider } from "../../hooks/useAuth";
 
-const fireAlert = (registration) => {
-  registration.showNotification("Hi there!", {
-    body: "This is a description with more details",
-    badge: "https://our-biz-app.netlify.app/meta/camera.png",
-    icon: "https://our-biz-app.netlify.app/meta/camera.png",
-    image: "https://our-biz-app.netlify.app/meta/cover-image.jpeg",
-    action: [
-      {
-        actions: "add",
-        tittle: "Add",
-      },
-      {
-        actions: "view",
-        tittle: "view",
-      },
-    ],
-  });
-};
+// const fireAlert = (registration) => {
+//   registration.showNotification("Hi there!", {
+//     body: "This is a description with more details",
+//     badge: "https://our-biz-app.netlify.app/meta/camera.png",
+//     icon: "https://our-biz-app.netlify.app/meta/camera.png",
+//     image: "https://our-biz-app.netlify.app/meta/cover-image.jpeg",
+//     action: [
+//       {
+//         actions: "add",
+//         tittle: "Add",
+//       },
+//       {
+//         actions: "view",
+//         tittle: "view",
+//       },
+//     ],
+//   });
+// };
 
-const getRegistration = async () => {
-  const registration = await navigator.serviceWorker.getRegistration();
+// const getRegistration = async () => {
+//   const registration = await navigator.serviceWorker.getRegistration();
 
-  if (Notification.permission === "granted") {
-    fireAlert(registration);
-  }
+//   if (Notification.permission === "granted") {
+//     fireAlert(registration);
+//   }
 
-  if (Notification.permission === "default") {
-    Notification.requestPermission(() => fireAlert(registration));
-  }
-};
+//   if (Notification.permission === "default") {
+//     Notification.requestPermission(() => fireAlert(registration));
+//   }
+// };
 
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/service-worker.js");
-  getRegistration();
-}
+// if ("serviceWorker" in navigator) {
+//   navigator.serviceWorker.register("/service-worker.js");
+//   getRegistration();
+// }
 
 const Global = createGlobalStyle`
 @font-face {
